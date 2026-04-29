028c67d2296d80d37f2ed12cc7b78138cf247386
Haseeb <haseebmine24@gmail.com>
2026-04-29 15:33:22 +0500

    first commit..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/projects/zong/lucky_draw/frontend/',
+  base: '/',   // ✅ FIXED HERE
   plugins: [
     react(),
     tailwindcss(),
